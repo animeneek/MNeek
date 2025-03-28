33fb3096bece549e0a231f06e8c886739fa5761c
animeneek <imemyanime@gmail.com>
2025-03-28 09:35:22 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Dynamically load the header
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-container').innerHTML = data;
+    });
+
 document.getElementById('home-button').addEventListener('click', function() {
     window.location.href = 'index.html';
 });
