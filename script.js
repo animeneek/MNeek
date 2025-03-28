3fab3c0df1d0b59c09a955a7cb950e942722bfc3
animeneek <imemyanime@gmail.com>
2025-03-28 14:44:02 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-// Dynamically load the header
+// Dynamically load the header and menu
 fetch('header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header-container').innerHTML = data;
+    });
+
+fetch('menu.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('menu-container').innerHTML = data;
+        const menuToggle = document.getElementById('menu-toggle');
+        const menu = document.querySelector('.menu');
+        
+        menuToggle.addEventListener('click', () => {
+            menu.classList.toggle('open');
+        });
     });
 
 document.getElementById('home-button').addEventListener('click', function() {
